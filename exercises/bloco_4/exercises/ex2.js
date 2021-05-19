5423f863ffb41d14f67debbd623a9893cf51595e
V. Dionysio <vini.dionysio@gmail.com>
2021-05-19 16:46:46 -0300

    exercises 1,2,3 - day 4.1

---
+++ exercises/bloco_4/exercises/ex2.js
@@ -0,0 +1,15 @@
+/* 
+Faça um programa que retorne o maior de dois números. 
+Defina no começo do programa duas variáveis com os valores que serão comparados.
+*/
+
+let a = 33;
+let b = 23;
+
+if (a === b) {
+  console.log("Números iguais: " + a);
+} else if (a > b) {
+  console.log("Maior número: " + a);
+} else {
+  console.log("Maior número: " + b);
+}
