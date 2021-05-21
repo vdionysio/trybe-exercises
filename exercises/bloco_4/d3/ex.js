9d469f0ab95b45a411c3bb13fc271364a4e7c037
V. Dionysio <vini.dionysio@gmail.com>
2021-05-21 15:53:33 -0300

    add exercises 4.3

---
+++ exercises/bloco_4/d3/ex.js
@@ -0,0 +1,43 @@
+/*
+1-Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+*/
+
+let n = 5;
+
+for (let i = 0; i < n; i += 1) {
+  console.log('*'.repeat(n));
+}
+
+/*
+2-Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+*/
+console.log('\n');
+
+n = 5;
+
+for (let i = n - 1; i >= 0; i -= 1) {
+    console.log('*'.repeat(n-i));
+}
+
+/*
+3-Agora inverta o lado do triângulo.
+*/
+console.log('\n');
+n = 8;
+
+for (let i = n - 1; i >= 0; i -= 1) {
+    console.log(' '.repeat(i) + '*'.repeat(n-i));
+}
+
+/*
+4-Depois, faça uma pirâmide com n asteriscos de base
+*/
+console.log('\n');
+
+n = 5;
+
+for (let i = n - 1; i >= 0; i -= 1) {
+  if ((n-i) % 2 !== 0) {
+    console.log(' '.repeat(i/2) + '*'.repeat(n-i) + ' '.repeat(i/2));
+  }
+}
