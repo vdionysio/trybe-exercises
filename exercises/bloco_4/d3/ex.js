b8cbe2199a6f8ef241cfa0e055e7e0bb44f58841
V. Dionysio <vini.dionysio@gmail.com>
2021-05-23 18:57:23 -0300

    remake exercises b4.3

--- exercises/bloco_4/d3/ex.js
+++ exercises/bloco_4/d3/ex.js
@@ -1,62 +1,83 @@
 /*
-1-Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
 */
 
 let n = 5;
+let line = '';
 
 for (let i = 0; i < n; i += 1) {
-  console.log('*'.repeat(n));
+  line += '*';
+}
+
+for (let i = 0; i < n; i += 1) {
+  console.log(line);
 }
 
 /*
-2-Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 */
-console.log('\n');
+console.log('\n')
+n = 5;
+line = '';
+
+for (let i = 0; i < n; i += 1) {
+  line += '*';
+  console.log(line);
+}
+
+
+/*
+3. Agora inverta o lado do triângulo
+*/
+console.log('\n')
 
 n = 5;
+line = '';
+let pos = n;
 
-for (let i = n - 1; i >= 0; i -= 1) {
-    console.log('*'.repeat(n-i));
+for (let i = 1; i <= n; i += 1) {
+  //1, 
+  for (let j = 1; j <= n; j += 1) {
+    if (j >= pos) {
+      line += '*';
+    }
+    else {
+      line += ' ';
+    }
+  }
+  
+  pos -= 1;
+  console.log(line);
+  line = '';
 }
 
+console.log(line);
 /*
-3-Agora inverta o lado do triângulo.
+4. Depois, faça uma pirâmide com n asteriscos de base
 */
-console.log('\n');
-n = 8;
+console.log('\n')
 
-for (let i = n - 1; i >= 0; i -= 1) {
-    console.log(' '.repeat(i) + '*'.repeat(n-i));
+n = 9;
+line = '';
+let mid = (n+1)/2;
+
+for (let i = 1; i <= mid; i += 1) {
+  //1, 
+  for (let j = 1; j <= n; j += 1) {
+    if (i == 1 && j == mid) {
+      line += '*';
+    }
+    else if (j > mid - i && j < mid + i){
+      line += '*';
+    }
+    else {
+      line += ' ';
+    }
+  }
+  
+
+  console.log(line);
+  line = '';
 }
 
-/*
-4-Depois, faça uma pirâmide com n asteriscos de base
-*/
-console.log('\n');
-
-n = 5;
-
-for (let i = n - 1; i >= 0; i -= 1) {
-  if ((n-i) % 2 !== 0) {
-    console.log(' '.repeat(i/2) + '*'.repeat(n-i) + ' '.repeat(i/2));
-  }
-}
-
-/*
-5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
-*/
-console.log('\n');
-
-n = 5;
-
-for (let i = n - 1; i >= 0; i -= 1) {
-  if (i === n - 1) {
-    console.log(' '.repeat(i/2) + '*'.repeat(n-i) + ' '.repeat(i/2));
-  }
-  else if (i === 0) {
-    console.log('*'.repeat(n));
-  }
-  else {
-    console.log(' '.repeat(Math.floor(i/2)) + '*' + ' '.repeat(i - 2*Math.floor(i/2)) + '*' + ' '.repeat(Math.floor(i/2)));
-  }
-}
+console.log(line);
