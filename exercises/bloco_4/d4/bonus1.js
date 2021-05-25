bb344b6113077692eb82a99240cb8df8830a1f42
V. Dionysio <vini.dionysio@gmail.com>
2021-05-25 12:03:17 -0300

    add exercicios bonus

--- exercises/bloco_4/d4/bonus1.js
+++ exercises/bloco_4/d4/bonus1.js
@@ -1,8 +1,9 @@
 //1. (Difícil) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
 
 function romanToArabic(roman) {
-
+  let strArr = [];
   let baseRoman = {
+    'I': 1,
     'V': 5,
     'X': 10,
     'L': 50,
@@ -10,13 +11,31 @@ function romanToArabic(roman) {
     'D': 500,
     'M': 1000
   }
+  let sum = 0;
 
   if (roman.length == 1) {
     return baseRoman[roman];
   }
   else {
-    if ()
+    for (let key in roman) {
+      strArr.push(roman[key]);
+    }
+    console.log(strArr);
+
+    for (let i = 0; i < strArr.length; i += 1) {
+      
+      if (baseRoman[strArr[i]] < baseRoman[strArr[i+1]]) {
+        sum = sum + (baseRoman[strArr[i+1]] - baseRoman[strArr[i]])
+        i += 1;
+      }
+      else {
+        sum = sum + baseRoman[strArr[i]]
+      }
+    }
   }
+  console.log(sum);
+
+  return(sum);
 }
 
-console.log(romanToArabic('X'))
+console.log(romanToArabic('LXI'));
---
+++ exercises/bloco_4/d4/bonus3.js
@@ -0,0 +1,56 @@
+/*
+A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+Copiar
+*/
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+
+function repeatTimes(arr) {
+  let info = {};
+  let results = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    info[arr[i]] = 0;
+  }
+
+  for (let key in info) {
+
+    for (let i = 0; i < arr.length; i += 1) {
+      if (key == arr[i]){
+        info[key] += 1;
+      }
+    }
+  }
+
+  return (info)
+}
+
+basketTotal = repeatTimes(basket);
+
+console.log(basketTotal)
+let result = 'Sua cesta possui: ';
+
+for (let key in basketTotal) {
+  result += basketTotal[key] + ' ' + key + 's' + ', '
+}
+
+console.log(result)
