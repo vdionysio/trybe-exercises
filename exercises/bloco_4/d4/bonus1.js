816189ec567b9a1559a3138322f9ef4e6e269142
V. Dionysio <vini.dionysio@gmail.com>
2021-05-25 10:21:53 -0300

    Exercícios b4.4

---
+++ exercises/bloco_4/d4/bonus1.js
@@ -0,0 +1,22 @@
+//1. (Difícil) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+function romanToArabic(roman) {
+
+  let baseRoman = {
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  if (roman.length == 1) {
+    return baseRoman[roman];
+  }
+  else {
+    if ()
+  }
+}
+
+console.log(romanToArabic('X'))
