6816d615cb4e3d33ad2024377f88b0362dbc24ce
V. Dionysio <vini.dionysio@gmail.com>
2021-07-13 18:10:09 -0300

    Exercises 10.1

---
+++ exercises/bloco_10/exercises/ex3.test.js
@@ -0,0 +1,32 @@
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+describe('testando a função myRemoveWithoutCopy(arr, item)', () => {
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1,2,4]);
+  })
+
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1,2,3,4])
+  })
+
+  test('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
+    const arr = [1,2,3,4];
+    myRemoveWithoutCopy(arr, 4)
+    expect(arr).not.toEqual([1,2,3,4]) //não é igual
+  })
+
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1,2,3,4])
+  })
+});
